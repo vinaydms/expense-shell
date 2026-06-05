dbeaf4c4c6f22236b1eae494011ddd884cf4e045
vinaydms <vinay.dms16@gmail.com>
2026-06-05 14:08:18 +0530

    mysql

--- mysql.sh
+++ mysql.sh
@@ -35,15 +35,15 @@ VALIDATE $? "Enabling Mysql server"
 systemctl start mysqld &>>LOGFILE
 VALIDATE $? "Starting Mysql server"
 
-mysql_secure_installation --set-root-pass ExpenseApp@1 &>>LOGFILE
-VALIDATE $? "Setting up root Password"
+# mysql_secure_installation --set-root-pass ExpenseApp@1 &>>LOGFILE
+# VALIDATE $? "Setting up root Password"
 
 #Below code will be useful for idempotent nature
-#  mysql -h db.devopsvinay.online -uroot -pExpenseApp@1 -e 'show databases;' &>>LOGFILE
-#  if [ $? -ne 0 ]
-#  then 
-#     mysql_secure_installation --set-root-pass ExpenseApp@1 &>>LOGFILE
-#     VALIDATE $? "MYSQL Root Password Setup"
-# else 
-#     echo -e "Mysql Root Password is already setup...$Y SKIPPING $N"
-# fi
+ mysql -h db.devopsvinay.online -uroot -pExpenseApp@1 -e 'show databases;' &>>LOGFILE
+ if [ $? -ne 0 ]
+ then 
+    mysql_secure_installation --set-root-pass ExpenseApp@1 &>>LOGFILE
+    VALIDATE $? "MYSQL Root Password Setup"
+else 
+    echo -e "Mysql Root Password is already setup...$Y SKIPPING $N"
+fi
