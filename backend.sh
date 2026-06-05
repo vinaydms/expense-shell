22339739050759622e7e4634cccaa9fd0c772abd
vinaydms <vinay.dms16@gmail.com>
2026-06-05 16:38:51 +0530

    mysql

--- backend.sh
+++ backend.sh
@@ -8,6 +8,9 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
+echo "please enter DB Password:"
+read -s mysql_root_password
+
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
@@ -44,16 +47,39 @@ else
     echo -e "Expense user already created...$Y SKIPPING $N"
 fi
 
-mkdir -p /app
+mkdir -p /app &>>LOGFILE
 VALIDATE $? "Creating app directory"
 
-curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip
+curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>LOGFILE
+VALIDATE $? "Downloading backend code"
 
 cd /app
-unzip /tmp/backend.zip
+unzip /tmp/backend.zip &>>LOGFILE
 VALIDATE $? "Extracted backend code"
 
-npm install
+npm install &>>LOGFILE
 VALIDATE $? "Installing nodejs dependencies"
+
+cp /home/ec2-user/expense-shell/backend.service /etc/systemd/system/backend.service &>>LOGFILE
+VALIDATE $? "Copied backend service"
+
+systemctl daemon-reload &>>LOGFILE
+VALIDATE $? "Daemon reload"
+
+systemctl start backend &>>LOGFILE
+VALIDATE $? "Starting backend"
+
+systemctl enable backend &>>LOGFILE
+VALIDATE $? "Enabling backend"
+
+dnf install mysql -y &>>LOGFILE
+VALIDATE $? "Installing Mysql client"
+
+mysql -h db.devopsvinay.online -uroot -p${mysql_root_password} < /app/schema/backend.sql &>>LOGFILE
+VALIDATE $? "Schema loading"
+
+systemctl restart backend &>>LOGFILE
+ VALIDATE $? "Restart backend"
+
 
 
