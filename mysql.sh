9db977b224a177c737b1ff7e309ec80f316ded5b
vinaydms <vinay.dms16@gmail.com>
2026-06-05 12:32:03 +0530

    mysql

--- mysql.sh
+++ mysql.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
-SCRIPT_NAME=$(echo $0 | cut "." -f1)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 R="\e[31m"
 G="\e[32m"
